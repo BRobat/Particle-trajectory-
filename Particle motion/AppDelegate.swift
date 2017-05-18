5ab9b01f0ef5e1204ac96a770a707ef37be5c407
BRobat <Patogeen@icloud.com>
2017-05-18 22:24:10 +0200

    Initial Commit

---
+++ Particle motion/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Particle motion
+//
+//  Created by Tohil on 18/05/2017.
+//  Copyright © 2017 Tohil. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
